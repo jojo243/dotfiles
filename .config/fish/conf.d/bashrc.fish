81489a5b4c50e0d27a01977a0b1aea7a3ef8a6be
Johannes Büttner <32260292+jojo243@users.noreply.github.com>
2021-11-05 20:41:56 +0100

    Create bashrc.fish

---
+++ .config/fish/conf.d/bashrc.fish
@@ -0,0 +1,12 @@
+if [ (cat /var/lib/dbus/machine-id) != f418507ffeb6301818923bfe5ec72b05 ]
+    # See http://wiki.bshg.com/x/WoMJCg for further information.
+    set -x http_proxy http://localhost:3128
+    set -x https_proxy http://localhost:3128
+    set -x no_proxy ".bshg.com, localhost, 127.0.0.0/8, ::1"
+
+    alias internet-start='bsh-proxy --start'
+    alias internet-stop='bsh-proxy --stop'
+    alias chromium-browser='chromium-browser --proxy-server="localhost:3128"'
+    alias bsh-guestversion='lsmod | grep -io vboxguest | xargs modinfo | grep -iw version'
+end
+
